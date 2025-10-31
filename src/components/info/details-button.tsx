192f5e25adb16d294d492807e609a7f041e90e0d
trino-troll <trino@mail.ru>
2025-10-31 11:11:09 +0300

    Фикс окна деталий на проде

--- src/components/info/details-button.tsx
+++ src/components/info/details-button.tsx
@@ -15,7 +15,7 @@ export function DetailsButton({ door }: { door: EcoshponPDK }) {
             </Button>
 
             {openDetails ? (
-                <div className="fixed inset-0 z-50 flex items-center justify-center h-full max-h-[80] overflow-auto">
+                <div className="fixed inset-0 z-50 flex items-center justify-center p-4">
                     <div
                         className="absolute inset-0 bg-black/50"
                         onClick={() => setOpenDetails(false)}
